3bb4d04a8742838d07a7c4f25d2b678f4072aaa2
James Espinosa <de.james013@gmail.com>
2024-08-13 14:32:39 +0800

    remove unnecessary handler

--- services/userService.js
+++ services/userService.js
@@ -1,22 +1,21 @@
-import asyncHandler from 'express-async-handler';
 import { User } from '../models/index.js';
 import Service from './service.js';
 
 class UserService extends Service {
   static model = User;
 
-  static updateUser = asyncHandler(async (id, body) => {
+  static updateUser = async (id, body) => {
     const data = User.filterFillables(body);
     if (data.password) data.password = await User.hashPassword(data.password);
     const user = await User.findByIdAndUpdate(id, data, { new: true });
     return user;
-  });
+  };
 
-  static authenticate = asyncHandler(async (email, password) => {
+  static authenticate = async (email, password) => {
     let user = await User.findOne({ email });
     if (!user || !(user && (await user.matchPassword(password)))) return null;
     return user;
-  });
+  };
 }
 
 export default UserService;
