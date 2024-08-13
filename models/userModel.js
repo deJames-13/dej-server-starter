e3e6714bad855b1192e39e2fc4b9200535a588f0
James Espinosa <de.james013@gmail.com>
2024-08-13 15:52:32 +0800

    fads

--- models/userModel.js
+++ models/userModel.js
@@ -37,11 +37,6 @@ User.pre('save', async function (next) {
 });
 
 User.methods.matchPassword = async function (password) {
-  console.log(
-    await bcrypt.compare(password, this.password),
-    password,
-    this.password
-  );
   return await bcrypt.compare(password, this.password);
 };
 
