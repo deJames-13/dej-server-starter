98fcfea0b47dedc8882cb9d9e53549f164483953
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-15 16:57:13 +0800

    adsf

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,14 @@
 import * as utils from '../utils/index.js';
 export default class Controller {
+  service;
+  resource;
   error = utils.errorHandler;
   success = utils.successHandler;
   validator = utils.validate;
-  service;
-  resource;
 
   // controller functions
   getALl = async (req, res) => {
-    const data = await this.service.getAll();
+    const data = await this.service?.getAll();
     if (!data.length) return this.error({ res, message: 'No data found!' });
 
     const resource = this.resource?.collection(data) || data;
@@ -16,7 +16,7 @@ export default class Controller {
   };
 
   getById = async (req, res) => {
-    const data = await this.service.getById(req.params.id);
+    const data = await this.service?.getById(req.params.id);
     if (!data?._id) return this.error({ res, message: 'Data not found!' });
 
     const resource = this.resource?.make(data) || data;
