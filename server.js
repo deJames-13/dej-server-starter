28b43902d59927857d0e6f321ce8568afa312ea8
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-17 00:11:32 +0800

    dsf

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
+import { connectDB, MONGO_URI, PORT } from '#config';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import path from 'path';
-import { connectDB } from './config/db.js';
-import { MONGO_URI, PORT } from './config/env.js';
 import * as err from './middlewares/errorMiddleware.js';
 import router from './routes/index.js';
 
-const server = () => {
-  const app = express();
-
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  app.use(cookieParser());
-  app.use('/api', router);
-
+const production = (app) => {
   if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     const root_folder = path.join(__dirname, '../');
@@ -29,15 +20,24 @@ const server = () => {
       res.send('API is running...');
     });
   }
+};
+
+const server = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+
+  app.use('/api', router);
+
+  production(app);
 
   app.use(err.notFound);
   app.use(err.errorHandler);
 
-  connectDB(MONGO_URI, () => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 };
-
-server();
+connectDB(MONGO_URI, server);
