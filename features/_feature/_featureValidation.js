5e5954aa9163377719eea210a4a36f4ae746749b
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-17 01:00:02 +0800

    asdf

--- features/_feature/_featureValidation.js
+++ features/_feature/_featureValidation.js
@@ -1,34 +1,18 @@
 import { check } from 'express-validator';
 
-const matchPassword = (value, { req }) => {
-  if (value !== req.body.password_confirmation)
-    throw new Error('Password does not match!');
-
-  return value;
-};
-
-const userCreateRules = () => {
+const _featureCreateRules = () => {
   // METHOD CHAINING
   return [
-    check('email').isEmail().withMessage('Email is invalid'),
-
     check('name')
       .notEmpty()
       .withMessage('Name is required')
       .matches(/^[a-zA-Z0-9 ]+$/)
       .withMessage('Name must be alphanumeric'),
-
-    check('password')
-      .isLength({ min: 6, max: 20 })
-      .withMessage('Password must be between 6 and 20 characters')
-      .custom(matchPassword),
   ];
 };
 
-const userUpdateRules = () => {
+const _featureUpdateRules = () => {
   return [
-    check('email').isEmail().withMessage('Email is invalid'),
-
     check('name')
       .optional()
       .isString()
@@ -36,36 +20,14 @@ const userUpdateRules = () => {
       .withMessage('Name is required')
       .matches(/^[a-zA-Z0-9 ]+$/)
       .withMessage('Name must be alphanumeric'),
-
-    check('password')
-      .optional()
-      .isLength({ min: 6, max: 20 })
-      .withMessage('Password must be between 6 and 20 characters')
-      .custom(matchPassword),
   ];
 };
 
-export { userCreateRules, userUpdateRules };
+export { _featureCreateRules, _featureUpdateRules };
 
 // // USING SCHEMA: BUT i don't like it
 // return checkSchema({
 //   name: {
 //     notEmpty: { errorMessage: 'Name is required!' },
-//   },
-//   email: {
-//     isEmail: { errorMessage: 'Email is invalid!' },
-//     notEmpty: { errorMessage: 'Email is required!' },
-//   },
-//   password: {
-//     isLength: {
-//       errorMessage: 'Password must be between 6 and 20 characters!',
-//       options: { min: 6, max: 20 },
-//     },
-//   },
-//   password_confirmation: {
-//     custom: {
-//       errorMessage: 'Password does not match!',
-//       options: matchPassword,
-//     },
 //   },
 // });
