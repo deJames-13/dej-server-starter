efec53e105a20d71df4387353232eb1b3b2c398a
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-17 11:51:17 +0800

    adsf

--- features/users/index.js
+++ features/users/index.js
@@ -1,5 +1,5 @@
 export { default as UserController } from './userController.js';
-export { default as User } from './userModel.js';
+export { default as UserModel } from './userModel.js';
 export { default as UserResource } from './userResource.js';
 export { default as userRoutes } from './userRoutes.js';
 export { default as UserService } from './userService.js';
