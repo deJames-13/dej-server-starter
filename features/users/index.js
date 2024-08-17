4f9c36db9d675c1765fdce6f254bc1d2f5a037b0
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-17 11:50:14 +0800

    df

--- features/users/index.js
+++ features/users/index.js
@@ -1,5 +1,6 @@
 export { default as UserController } from './userController.js';
 export { default as User } from './userModel.js';
+export { default as UserResource } from './userResource.js';
 export { default as userRoutes } from './userRoutes.js';
 export { default as UserService } from './userService.js';
 export * from './userValidation.js';
