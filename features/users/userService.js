7baae8c87805ffd10ecbcba0f8f0bf9f2f4fe92d
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-17 11:54:26 +0800

    dsf

--- features/users/userService.js
+++ features/users/userService.js
@@ -1,9 +1,9 @@
 import { Service } from '#lib';
 import { destroyToken, generateToken } from '#utils';
-import User from './userModel.js';
+import UserModel from './userModel.js';
 
 class UserService extends Service {
-  model = User;
+  model = UserModel;
   authToken = 'jwt';
 
   async registerUser(body) {
@@ -16,7 +16,7 @@ class UserService extends Service {
   }
 
   async authenticate(email, password) {
-    let user = await User.findOne({ email });
+    let user = await this.model?.findOne({ email });
     if (!user || !(user && (await user.matchPassword(password)))) user = null;
     const token = user && generateToken(user._id, this.authToken);
 
@@ -34,9 +34,10 @@ class UserService extends Service {
     });
     if (userExists) throw new Error('User with that email already exists!');
 
-    const data = User.filterFillables(body);
-    if (data.password) data.password = await User.hashPassword(data.password);
-    const user = await User.findByIdAndUpdate(id, data, { new: true });
+    const data = this.model?.filterFillables(body);
+    if (data.password)
+      data.password = await this.model?.hashPassword(data.password);
+    const user = await this.model?.findByIdAndUpdate(id, data, { new: true });
     return user;
   }
 }
